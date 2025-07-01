fc04ac5e0eb25b40e67f0c7eb22475e21e8715b3
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-01 21:51:16 +0200

    repositories-commit

--- src/main/java/com/example/demo/repository/ProductRepository.java
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -1,2 +1,9 @@
-package com.example.demo.repository;public interface ProductRepository {
+package com.example.demo.repository;
+
+import com.example.demo.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository <Product, Long> {
 }
