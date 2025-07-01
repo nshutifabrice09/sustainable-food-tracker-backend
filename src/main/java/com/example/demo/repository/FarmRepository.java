3c7f0e7340b8d1bd532c39ae70fc234029fea259
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-01 21:49:34 +0200

    repositories-commit

--- src/main/java/com/example/demo/repository/FarmRepository.java
+++ src/main/java/com/example/demo/repository/FarmRepository.java
@@ -1,2 +1,9 @@
-package com.example.demo.repository;public interface FarmRepository {
+package com.example.demo.repository;
+
+import com.example.demo.model.Farm;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FarmRepository extends JpaRepository <Farm, Long> {
 }
--- src/main/java/com/example/demo/repository/HarvestRecordRepository.java
+++ src/main/java/com/example/demo/repository/HarvestRecordRepository.java
@@ -1,2 +1,9 @@
-package com.example.demo.repository;public interface HarvestRecordRepository {
+package com.example.demo.repository;
+
+import com.example.demo.model.HarvestRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HarvestRecordRepository extends JpaRepository <HarvestRecord, Long> {
 }
