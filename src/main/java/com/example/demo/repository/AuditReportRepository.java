5f480aae9add8098d2e3221357e1967ae5cb82f8
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-01 21:14:49 +0200

    initial-repositories-commit

--- src/main/java/com/example/demo/repository/AuditReportRepository.java
+++ src/main/java/com/example/demo/repository/AuditReportRepository.java
@@ -1,2 +1,9 @@
-package com.example.demo.repository;public interface AuditReportRepository {
+package com.example.demo.repository;
+
+import com.example.demo.model.AuditReport;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuditReportRepository extends JpaRepository <AuditReport, Long> {
 }
