71f34dc55db50b247a5b3b5041b0873579775369
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-02 10:08:01 +0200

    initial-certificateServiceImpl-commit

--- src/main/java/com/example/demo/service/CertificateService.java
+++ src/main/java/com/example/demo/service/CertificateService.java
@@ -1,5 +1,13 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Certificate;
+
+import java.util.List;
+
 public interface CertificateService {
-    
+    List<Certificate> getAllCertificates();
+    Certificate getCertificateById(Long id);
+    Certificate saveCertificate(Certificate certificate, Long farmId);
+    Certificate updateCertificate(Long id, Certificate certificate);
+    void removeCertificate(Long id);
 }
