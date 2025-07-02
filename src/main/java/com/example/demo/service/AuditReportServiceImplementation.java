5e8f7e85b9adfc6bcd142b6ab25c76beb27503c5
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-02 09:25:08 +0200

    initial-batchService-commit

--- src/main/java/com/example/demo/service/AuditReportServiceImplementation.java
+++ src/main/java/com/example/demo/service/AuditReportServiceImplementation.java
@@ -1,7 +1,9 @@
 package com.example.demo.service;
 
 import com.example.demo.model.AuditReport;
+import com.example.demo.model.Farm;
 import com.example.demo.repository.AuditReportRepository;
+import com.example.demo.repository.FarmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +13,12 @@ import java.util.List;
 public class AuditReportServiceImplementation implements AuditReportService{
 
     private final AuditReportRepository auditReportRepository;
+    private final FarmRepository farmRepository;
 
     @Autowired
-    public AuditReportServiceImplementation(AuditReportRepository auditReportRepository){
+    public AuditReportServiceImplementation(AuditReportRepository auditReportRepository, FarmRepository farmRepository){
         this.auditReportRepository = auditReportRepository;
+        this.farmRepository= farmRepository;
     }
 
     @Override
@@ -29,16 +33,26 @@ public class AuditReportServiceImplementation implements AuditReportService{
 
     @Override
     public AuditReport saveAuditReport(AuditReport auditReport, Long farmId) {
-        return null;
+        Farm farm = farmRepository.findFarmById(farmId);
+        auditReport.setFarm(farm);
+        return auditReportRepository.save(auditReport);
     }
 
     @Override
     public AuditReport updateAuditReport(Long id, AuditReport auditReport) {
+        AuditReport existAuditReport = auditReportRepository.findAuditReportById(id);
+        if(existAuditReport != null){
+            existAuditReport.setAuditorName(auditReport.getAuditorName());
+            existAuditReport.setAuditDate(auditReport.getAuditDate());
+            existAuditReport.setReportUrl(auditReport.getReportUrl());
+            existAuditReport.setPassed(auditReport.isPassed());
+            return auditReportRepository.save(existAuditReport);
+        }
         return null;
     }
 
     @Override
     public void removeAuditReport(Long id) {
-
+        auditReportRepository.deleteById(id);
     }
 }
