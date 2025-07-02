fad65d00caa7ff763971879f19ee7044aba3525d
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-02 09:16:02 +0200

    repository-update

--- src/main/java/com/example/demo/service/AuditReportServiceImplementation.java
+++ src/main/java/com/example/demo/service/AuditReportServiceImplementation.java
@@ -1,2 +1,34 @@
-package com.example.demo.service;public class AuditReportServiceImplementation {
+package com.example.demo.service;
+
+import com.example.demo.model.AuditReport;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AuditReportServiceImplementation implements AuditReportService{
+    @Override
+    public List<AuditReport> getAllAuditReports() {
+        return null;
+    }
+
+    @Override
+    public AuditReport getAuditReportById(Long id) {
+        return null;
+    }
+
+    @Override
+    public AuditReport saveAuditReport(AuditReport auditReport, Long farmId) {
+        return null;
+    }
+
+    @Override
+    public AuditReport updateAuditReport(Long id, AuditReport auditReport) {
+        return null;
+    }
+
+    @Override
+    public void removeAuditReport(Long id) {
+
+    }
 }
