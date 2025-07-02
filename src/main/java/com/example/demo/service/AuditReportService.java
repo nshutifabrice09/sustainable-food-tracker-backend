97b574aff28fc5af871a8a4300d2beda69e8dea5
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-02 09:13:00 +0200

    initial-service-impl-commit

--- src/main/java/com/example/demo/service/AuditReportService.java
+++ src/main/java/com/example/demo/service/AuditReportService.java
@@ -1,2 +1,13 @@
-package com.example.demo.service;public interface AuditReportService {
+package com.example.demo.service;
+
+import com.example.demo.model.AuditReport;
+
+import java.util.List;
+
+public interface AuditReportService {
+    List<AuditReport> getAllAuditReports();
+    AuditReport getAuditReportById(Long id);
+    AuditReport saveAuditReport(AuditReport auditReport, Long farmId);
+    AuditReport updateAuditReport(Long id, AuditReport auditReport);
+    void removeAuditReport(Long id);
 }
