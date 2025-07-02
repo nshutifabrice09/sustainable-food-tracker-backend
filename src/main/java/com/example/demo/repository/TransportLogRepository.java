537442b5188323205b41cbadba94150d8a66ef1c
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-02 09:12:02 +0200

    initial-service-commit

--- src/main/java/com/example/demo/repository/TransportLogRepository.java
+++ src/main/java/com/example/demo/repository/TransportLogRepository.java
@@ -1,2 +1,9 @@
-package com.example.demo.repository;public interface TransportLogRepository {
+package com.example.demo.repository;
+
+import com.example.demo.model.TransportLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransportLogRepository extends JpaRepository <TransportLog, Long> {
 }
