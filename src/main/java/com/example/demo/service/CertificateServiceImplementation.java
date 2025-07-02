d418b418ec670e57803b4f9656e08aae24ae43c2
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-02 10:09:53 +0200

    certificateServiceImpl-constructor-updates

--- src/main/java/com/example/demo/service/CertificateServiceImplementation.java
+++ src/main/java/com/example/demo/service/CertificateServiceImplementation.java
@@ -1,8 +1,35 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Certificate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CertificateServiceImplementation implements CertificateService{
 
+    @Override
+    public List<Certificate> getAllCertificates() {
+        return null;
+    }
+
+    @Override
+    public Certificate getCertificateById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Certificate saveCertificate(Certificate certificate, Long farmId) {
+        return null;
+    }
+
+    @Override
+    public Certificate updateCertificate(Long id, Certificate certificate) {
+        return null;
+    }
+
+    @Override
+    public void removeCertificate(Long id) {
+
+    }
 }
