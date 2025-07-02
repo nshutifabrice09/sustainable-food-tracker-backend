fb60218389a0a5e75cedc39399bf31788794dc54
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-02 09:30:43 +0200

    Initial-batchServiceImpl-commit

--- src/main/java/com/example/demo/service/BatchService.java
+++ src/main/java/com/example/demo/service/BatchService.java
@@ -1,5 +1,13 @@
 package com.example.demo.service;
 
-public interface BatchService {
+import com.example.demo.model.Batch;
 
+import java.util.List;
+
+public interface BatchService {
+    List<Batch> getAllBatches();
+    Batch getBatchById(Long id);
+    Batch saveBatch(Batch batch, Long productId, Long farmId);
+    Batch updateBatch(Long id, Batch batch);
+    void removeBatch(Long id);
 }
