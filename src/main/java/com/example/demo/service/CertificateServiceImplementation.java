3829379a2aebf6cbea451fc3b34de60032fff16e
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-07 09:51:14 +0200

    initial-certificateServiveImpl-commit

--- src/main/java/com/example/demo/service/CertificateServiceImplementation.java
+++ src/main/java/com/example/demo/service/CertificateServiceImplementation.java
@@ -1,12 +1,25 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Certificate;
+import com.example.demo.repository.CertificateRepository;
+import com.example.demo.repository.FarmRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CertificateServiceImplementation implements CertificateService{
+
+    private final CertificateRepository certificateRepository;
+    private final FarmRepository farmRepository;
+
+    @Autowired
+    public CertificateServiceImplementation(CertificateRepository certificateRepository, FarmRepository farmRepository) {
+        this.certificateRepository = certificateRepository;
+        this.farmRepository = farmRepository;
+    }
+
 
     @Override
     public List<Certificate> getAllCertificates() {
