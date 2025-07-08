5c74d5894081bec341a869792f7a74beaca8b9e0
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-08 10:16:18 +0200

    initial-farmServiceImpl-commit

--- src/main/java/com/example/demo/service/FarmService.java
+++ src/main/java/com/example/demo/service/FarmService.java
@@ -1,2 +1,13 @@
-package com.example.demo.service;public interface FarmService {
+package com.example.demo.service;
+
+import com.example.demo.model.Farm;
+
+import java.util.List;
+
+public interface FarmService {
+    List<Farm> getAllFarms();
+    Farm getFarmById(Long id);
+    Farm saveFarm (Farm farm, Long userId);
+    Farm updateFarm (Long id, Farm farm);
+    void removeFarm (Long id);
 }
