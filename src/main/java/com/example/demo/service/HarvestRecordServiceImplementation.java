ab0febbeb4cdb3a693546a77bb7d85e3d5ee2b3d
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-08 10:49:31 +0200

    harvestRecordServiceIMpl-commit-update

--- src/main/java/com/example/demo/service/HarvestRecordServiceImplementation.java
+++ src/main/java/com/example/demo/service/HarvestRecordServiceImplementation.java
@@ -1,7 +1,48 @@
 package com.example.demo.service;
 
+import com.example.demo.model.HarvestRecord;
+import com.example.demo.repository.BatchRepository;
+import com.example.demo.repository.HarvestRecordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class HarvestRecordServiceImplementation implements HarvestRecordService{
+
+    private final HarvestRecordRepository harvestRecordRepository;
+    private final BatchRepository batchRepository;
+
+    @Autowired
+    public HarvestRecordServiceImplementation(HarvestRecordRepository harvestRecordRepository, BatchRepository batchRepository) {
+        this.harvestRecordRepository = harvestRecordRepository;
+        this.batchRepository = batchRepository;
+    }
+
+
+    @Override
+    public List<HarvestRecord> getAllHarvestRecords() {
+        return null;
+    }
+
+    @Override
+    public HarvestRecord getHarvestRecordById(Long id) {
+        return null;
+    }
+
+    @Override
+    public HarvestRecord saveHarvestRecord(HarvestRecord harvestRecord, Long batchId) {
+        return null;
+    }
+
+    @Override
+    public HarvestRecord updateHarvestRecord(Long id, HarvestRecord harvestRecord) {
+        return null;
+    }
+
+    @Override
+    public void removeHarvestRecord(Long id) {
+
+    }
 }
