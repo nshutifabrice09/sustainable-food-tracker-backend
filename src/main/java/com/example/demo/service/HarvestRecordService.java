e452fa924e4dc1d176d02657e5bd7402463b756e
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-08 10:47:11 +0200

    initial-harvestRecordServiceIMpl-commit

--- src/main/java/com/example/demo/service/HarvestRecordService.java
+++ src/main/java/com/example/demo/service/HarvestRecordService.java
@@ -1,2 +1,14 @@
-package com.example.demo.service;public interface HarvestRecordService {
+package com.example.demo.service;
+
+import com.example.demo.model.HarvestRecord;
+
+import java.util.List;
+
+public interface HarvestRecordService {
+    List<HarvestRecord> getAllHarvestRecords();
+    HarvestRecord getHarvestRecordById(Long id);
+    HarvestRecord saveHarvestRecord(HarvestRecord harvestRecord, Long batchId);
+    HarvestRecord updateHarvestRecord(Long id, HarvestRecord harvestRecord);
+    void removeHarvestRecord (Long id);
+
 }
