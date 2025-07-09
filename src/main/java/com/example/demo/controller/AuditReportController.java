576abfc6cccd4400314730dd97565a1ab08364a1
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-09 17:18:34 +0200

    second-auditReportController-commit

--- src/main/java/com/example/demo/controller/AuditReportController.java
+++ src/main/java/com/example/demo/controller/AuditReportController.java
@@ -1,2 +1,4 @@
-package com.example.demo.controller;public class AuditReportController {
+package com.example.demo.controller;
+
+public class AuditReportController {
 }
--- src/main/java/com/example/demo/service/UserServiceImplementation.java
+++ src/main/java/com/example/demo/service/UserServiceImplementation.java
@@ -1,7 +1,50 @@
 package com.example.demo.service;
 
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserServiceImplementation implements UserService{
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImplementation(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userRepository.findUserById(id);
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User updateUser(Long id, User user) {
+        User existUser = userRepository.findUserById(id);
+        if(existUser != null){
+            existUser.setUsername(user.getUsername());
+            existUser.setEmail(user.getEmail());
+            existUser.setPassword(user.getPassword());
+            return userRepository.save(existUser);
+        }
+        return null;
+    }
+    @Override
+    public void removeUser(Long id) {
+        userRepository.deleteById(id);
+    }
 }
