1085020272db867a9ee0bffea4a53fe678176e44
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-09 13:02:28 +0200

    final-practiceRecordService-Impl-commit-update

--- src/main/java/com/example/demo/service/PracticeRecordServiceImplementation.java
+++ src/main/java/com/example/demo/service/PracticeRecordServiceImplementation.java
@@ -1,7 +1,34 @@
 package com.example.demo.service;
 
+import com.example.demo.model.PracticeRecord;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PracticeRecordServiceImplementation implements PracticeRecordService{
+    @Override
+    public List<PracticeRecord> getAllPracticeRecords() {
+        return null;
+    }
+
+    @Override
+    public PracticeRecord getPracticeRecordById(Long id) {
+        return null;
+    }
+
+    @Override
+    public PracticeRecord savePracticeRecord(PracticeRecord practiceRecord, Long batchId) {
+        return null;
+    }
+
+    @Override
+    public PracticeRecord updatePracticeRecord(Long id, PracticeRecord practiceRecord) {
+        return null;
+    }
+
+    @Override
+    public void removePracticeRecord(Long id) {
+
+    }
 }
