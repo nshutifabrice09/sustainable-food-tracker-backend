5c47aa5e1df9e11f7304584119413df100c718b1
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-09 18:08:17 +0200

    model-update-commit

--- src/main/java/com/example/demo/controller/CertificateController.java
+++ src/main/java/com/example/demo/controller/CertificateController.java
@@ -1,2 +1,19 @@
-package com.example.demo.controller;public class CertificateController {
+package com.example.demo.controller;
+
+import com.example.demo.service.CertificateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class CertificateController {
+
+    private final CertificateService certificateService;
+
+
+    @Autowired
+    public CertificateController(CertificateService certificateService) {
+        this.certificateService = certificateService;
+    }
 }
