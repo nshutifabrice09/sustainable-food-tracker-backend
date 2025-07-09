60be8e1f12af89f83e2d0cfc5b71ae27f3e10e69
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-09 13:27:21 +0200

    final-productServiceImpl-commit-update

--- src/main/java/com/example/demo/service/ProductServiceImplementation.java
+++ src/main/java/com/example/demo/service/ProductServiceImplementation.java
@@ -1,7 +1,45 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Product;
+import com.example.demo.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductServiceImplementation implements ProductService{
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductServiceImplementation(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+
+    @Override
+    public List<Product> getAllProducts() {
+        return null;
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Product saveProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product updateProduct(Long id, Product product) {
+        return null;
+    }
+
+    @Override
+    public void removeProduct(Long id) {
+
+    }
 }
