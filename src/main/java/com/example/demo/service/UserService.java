004cc81c784e7c760ba0e84a032a9945f5768f3c
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-09 16:38:00 +0200

    initial-userServiceImpl-commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,2 +1,13 @@
-package com.example.demo.service;public interface UserService {
+package com.example.demo.service;
+
+import com.example.demo.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> getAllUsers();
+    User getUserById(Long id);
+    User saveUser(User user);
+    User updateUser(Long id, User user);
+    void removeUser (Long id);
 }
