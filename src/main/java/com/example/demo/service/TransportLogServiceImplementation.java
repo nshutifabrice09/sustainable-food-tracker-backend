c3e0c6d5cdff9a2f760649b02e91942882185c36
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-09 14:24:53 +0200

    final-transportLogServiceIMpl-commit

--- src/main/java/com/example/demo/service/TransportLogServiceImplementation.java
+++ src/main/java/com/example/demo/service/TransportLogServiceImplementation.java
@@ -1,7 +1,34 @@
 package com.example.demo.service;
 
+import com.example.demo.model.TransportLog;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TransportLogServiceImplementation implements TransportLogService{
+    @Override
+    public List<TransportLog> getAllTransportLogs() {
+        return null;
+    }
+
+    @Override
+    public TransportLog getTransportLogById(Long id) {
+        return null;
+    }
+
+    @Override
+    public TransportLog saveTransportLog(TransportLog transportLog, Long batchId) {
+        return null;
+    }
+
+    @Override
+    public TransportLog updateTransportLog(Long id, TransportLog transportLog) {
+        return null;
+    }
+
+    @Override
+    public void removeTransportLog(Long id) {
+
+    }
 }
