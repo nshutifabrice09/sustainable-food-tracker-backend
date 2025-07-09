766eed4237e0910beb4d352c5c5a05742a07ae56
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-09 12:55:31 +0200

    initial-practiceRecordService-Impl-commit

--- src/main/java/com/example/demo/service/PracticeRecordService.java
+++ src/main/java/com/example/demo/service/PracticeRecordService.java
@@ -1,2 +1,13 @@
-package com.example.demo.service;public interface PracticeRecordService {
+package com.example.demo.service;
+
+import com.example.demo.model.PracticeRecord;
+
+import java.util.List;
+
+public interface PracticeRecordService {
+    List<PracticeRecord> getAllPracticeRecords();
+    PracticeRecord getPracticeRecordById(Long id);
+    PracticeRecord savePracticeRecord (PracticeRecord practiceRecord, Long batchId);
+    PracticeRecord updatePracticeRecord (Long id, PracticeRecord practiceRecord);
+    void removePracticeRecord(Long id);
 }
