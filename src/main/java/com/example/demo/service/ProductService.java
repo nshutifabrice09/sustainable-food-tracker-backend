c9f0d060e5b07aa469d1ab452454c844342d47f4
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-09 13:13:33 +0200

    initial-productServiceImpl-commit

--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -1,2 +1,13 @@
-package com.example.demo.service;public interface ProductService {
+package com.example.demo.service;
+
+import com.example.demo.model.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    List<Product> getAllProducts();
+    Product getProductById(Long id);
+    Product saveProduct (Product product);
+    Product updateProduct(Long id, Product product);
+    void removeProduct(Long id);
 }
