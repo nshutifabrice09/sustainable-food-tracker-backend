f9134aa085a45e30435cfe78c6813ece26d8a707
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-09 14:16:52 +0200

    initial-transportLogServiceIMpl-commit

--- src/main/java/com/example/demo/service/TransportLogService.java
+++ src/main/java/com/example/demo/service/TransportLogService.java
@@ -1,2 +1,13 @@
-package com.example.demo.service;public interface TransportLogService {
+package com.example.demo.service;
+
+import com.example.demo.model.TransportLog;
+
+import java.util.List;
+
+public interface TransportLogService {
+    List<TransportLog> getAllTransportLogs();
+    TransportLog getTransportLogById(Long id);
+    TransportLog saveTransportLog(TransportLog transportLog, Long batchId);
+    TransportLog updateTransportLog(Long id, TransportLog transportLog);
+    void removeTransportLog(Long id);
 }
