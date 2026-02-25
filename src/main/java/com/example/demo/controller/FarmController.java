e2c934df95dbcc5447ef52cd342140547f8108e3
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2026-02-25 15:04:43 +0200

    updated application.properties

--- src/main/java/com/example/demo/controller/FarmController.java
+++ src/main/java/com/example/demo/controller/FarmController.java
@@ -1,2 +1,30 @@
-package com.example.demo.controller;public class FarmController {
+package com.example.demo.controller;
+
+import com.example.demo.model.Farm;
+import com.example.demo.service.FarmService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class FarmController {
+
+    private final FarmService farmService;
+
+    @Autowired
+    public FarmController(FarmService farmService) {
+        this.farmService = farmService;
+    }
+
+    @PostMapping("/farm/{userId}")
+    public Farm saveFarm(@RequestBody Farm farm, @PathVariable ("userId") Long userId){
+        return farmService.saveFarm(farm, userId);
+    }
+
+    @GetMapping("/farms")
+    public List<Farm> farmList(){
+        return farmService.getAllFarms();
+    }
 }
